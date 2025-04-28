c1a9a570612d8a903dc78d9ddf319542d847d245
aoda <aoda2826@qq.com>
2025-04-29 07:27:54 +0800

    fix import path in document.controller.ts

--- services/gateway/src/modules/Document/document.controller.ts
+++ services/gateway/src/modules/Document/document.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Headers } from '@nestjs/common'
 import { MicroServiceNames } from '@shared/constants/constant'
 import DocumentService from './document.service'
-import NoToken from '@modules/Auth/middlewares/noToken.decorator'
+import NoToken from '@shared/decorators/noToken.decorator'
 
 @Controller(MicroServiceNames.DOCUMENT)
 export class DocumentController {
