f1d4dd2a07e7fcd411e54948bf43cb5516534e36
aoda <aoda2826@qq.com>
2025-05-02 14:12:03 +0800

    Refactor document controller and service to remove locale parameter from default trip views endpoint

--- services/gateway/src/modules/Document/document.service.ts
+++ services/gateway/src/modules/Document/document.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import HttpClientService from '@shared/core/httpClient/HttpClient.service'
+import { HttpService } from '@nestjs/axios'
 
 @Injectable()
 export default class DocumentService {
     private readonly httpClient: ReturnType<HttpClientService['create']>
     constructor(
         private http: HttpClientService,
-        private configService: ConfigService
+        private configService: ConfigService,
+        private httpService: HttpService
     ) {
         this.httpClient = this.http.create(this.configService.get('documentService.baseURL'))
     }
     getDefaultTripViews = async () => {
         try {
-            return await this.httpClient.get('file/v1/default-trip-views')
+            // return await this.httpClient.get('file/v1/default-trip-views')
+            return await this.httpService.get('http://ms-document-service-uat.fullstack-namespace-uat.svc.cluster.local/api/document/file/v1/default-trip-views')
+
         } catch (error) {
             throw new Error(`Error fetching default trip views: ${error}`)
         }
